3175d4994ebb34cf062e88c1f2e6484810b78380
Nikolay Karadzhov <35952928+NikolayKaradzhov@users.noreply.github.com>
2019-01-28 10:00:17 +0200

    Update 11.Multiplication Table 2.0.cs

--- Intro and Basic Syntax - Lab/11.Multiplication Table 2.0.cs
+++ Intro and Basic Syntax - Lab/11.Multiplication Table 2.0.cs
@@ -1,1 +1,24 @@
+using System;
 
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = int.Parse(Console.ReadLine());
+
+            int multiplier = int.Parse(Console.ReadLine());
+
+            for (int i = multiplier; i <= 10; i++)
+            { 
+                    int result = input * i;
+                    Console.WriteLine($"{input} X {i} = {result}");
+            }
+            if (multiplier > 10)
+            {
+                Console.WriteLine($"{input} X {multiplier} = {input * multiplier}");
+            }
+        }
+    }
+}
