de7075b49b97c66122a650ed5463fcdc98b76aee
Nikolay Karadzhov <35952928+NikolayKaradzhov@users.noreply.github.com>
2019-01-28 11:15:58 +0200

    Update 10. Rage Expenses.cs

--- Intro and Basic Syntax - Exercise/10. Rage Expenses.cs
+++ Intro and Basic Syntax - Exercise/10. Rage Expenses.cs
@@ -1,1 +1,54 @@
+using System;
+using System.Globalization;
 
+class TriangleOfNumbers
+{
+    static void Main()
+    {
+        int lostGamesCount = int.Parse(Console.ReadLine());
+        double headsetPrice = double.Parse(Console.ReadLine());
+        double mousePrice = double.Parse(Console.ReadLine());
+        double keyboardPrice = double.Parse(Console.ReadLine());
+        double displayPrice = double.Parse(Console.ReadLine());
+
+        int trashedHeadsetTimes = 0;
+        int trashedMouseTimes = 0;
+        int trashedKeyboardTimes = 0;
+        int trashedDisplayTimes = 0;
+
+        double rageExpenseMouse = 0.0;
+        double rageExpenseKeyBoard = 0.0;
+        double rageExpenseHeadseat = 0.0;
+        double rageExpenseDisplay = 0.0;
+
+        for (int currentGame = 1; currentGame <= lostGamesCount; currentGame++)
+        {
+            if (currentGame % 2 == 0)
+            {
+                trashedHeadsetTimes++;
+            }
+            if (currentGame % 3 == 0)
+            {   
+                trashedMouseTimes++;
+            }
+            if (currentGame % 6 == 0)
+            {
+                trashedKeyboardTimes++;
+            }
+            if (currentGame % 12 == 0)
+            {
+                trashedDisplayTimes++;
+            }
+        }
+        rageExpenseHeadseat = (double)trashedHeadsetTimes * headsetPrice;
+        rageExpenseMouse = (double)trashedMouseTimes * mousePrice;
+        rageExpenseKeyBoard = (double)trashedKeyboardTimes * keyboardPrice;
+        rageExpenseDisplay = (double)trashedDisplayTimes * displayPrice;
+
+        double totalRageExpenses = rageExpenseDisplay
+                + rageExpenseHeadseat
+                + rageExpenseKeyBoard
+                + rageExpenseMouse;
+            Console.WriteLine($"Rage expenses: {totalRageExpenses:f2} lv.");
+    }
+}
