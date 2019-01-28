13ed72c491e8460d2b37d423d5a7986f418cef82
Nikolay Karadzhov <35952928+NikolayKaradzhov@users.noreply.github.com>
2019-01-28 11:15:09 +0200

    Update 08. Triangle of Numbers.cs

--- Intro and Basic Syntax - Exercise/08. Triangle of Numbers.cs
+++ Intro and Basic Syntax - Exercise/08. Triangle of Numbers.cs
@@ -1,1 +1,19 @@
+using System;
+using System.Globalization;
 
+class TriangleOfNumbers
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        for (int row = 1; row <= n; row++)
+        {
+            for (int col = 1; col <= row; col++)
+            {
+                Console.Write($"{row} ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
