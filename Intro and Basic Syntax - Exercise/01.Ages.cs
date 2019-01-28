ce02afcb2fcb870309abf439d6758265b24e61d8
Nikolay Karadzhov <35952928+NikolayKaradzhov@users.noreply.github.com>
2019-01-28 11:11:23 +0200

    Update 01.Ages.cs

--- Intro and Basic Syntax - Exercise/01.Ages.cs
+++ Intro and Basic Syntax - Exercise/01.Ages.cs
@@ -1,2 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-dsaasdas
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ageInput = int.Parse(Console.ReadLine());
+
+            if (ageInput >= 0 && ageInput <= 2)
+            {
+                Console.WriteLine("baby");
+            }
+            else if (ageInput >= 3 && ageInput <= 13)
+            {
+                Console.WriteLine("child");
+            }
+            else if (ageInput >= 14 && ageInput <= 19)
+            {
+                Console.WriteLine("teenager");
+            }
+            else if (ageInput >= 20 && ageInput <= 65)
+            {
+                Console.WriteLine("adult");
+            }
+            else if (ageInput > 65)
+            {
+                Console.WriteLine("elder");
+            }
+        }
+    }
+}
