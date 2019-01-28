2d8208e8075175d1704013577f7af013dec3508c
Nikolay Karadzhov <35952928+NikolayKaradzhov@users.noreply.github.com>
2019-01-28 09:59:31 +0200

    Update 09.Sum of Odd Numbers.cs

--- Intro and Basic Syntax - Lab/09.Sum of Odd Numbers.cs
+++ Intro and Basic Syntax - Lab/09.Sum of Odd Numbers.cs
@@ -1,1 +1,24 @@
+using System;
 
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int sumOddNumbers = 0;
+           
+                for (int i = 1; i <= n * 2; i++)
+                {
+                if (i % 2 == 1)
+                {
+                    Console.WriteLine(i);
+                    sumOddNumbers += i;
+                }
+                }
+            Console.WriteLine($"Sum: {sumOddNumbers}");
+        }
+    }
+}
