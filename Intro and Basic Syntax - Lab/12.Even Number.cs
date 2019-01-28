659401d81b02a10bce1b3ea4c366c8ba0f6faba2
Nikolay Karadzhov <35952928+NikolayKaradzhov@users.noreply.github.com>
2019-01-28 10:00:52 +0200

    Update 12.Even Number.cs

--- Intro and Basic Syntax - Lab/12.Even Number.cs
+++ Intro and Basic Syntax - Lab/12.Even Number.cs
@@ -1,1 +1,25 @@
+using System;
 
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {  
+            while (true)
+            {
+                int evenNumberInput = int.Parse(Console.ReadLine());
+
+                if (evenNumberInput % 2 != 0)
+                {
+                    Console.WriteLine("Please write an even number.");
+                }
+                else
+                {
+                    Console.WriteLine($"The number is: {Math.Abs(evenNumberInput)}");
+                    break;
+                }            
+            }
+        }
+    }
+}
