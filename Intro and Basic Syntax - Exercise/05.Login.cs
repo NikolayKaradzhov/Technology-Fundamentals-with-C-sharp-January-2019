45c0c49f1bc5df2a9486c8426ad5f2e8ae5f59b3
Nikolay Karadzhov <35952928+NikolayKaradzhov@users.noreply.github.com>
2019-01-28 11:14:02 +0200

    Update 05.Login.cs

--- Intro and Basic Syntax - Exercise/05.Login.cs
+++ Intro and Basic Syntax - Exercise/05.Login.cs
@@ -1,1 +1,51 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string passwordInput = Console.ReadLine();
+
+            string reversedInput = string.Empty;
+
+            int counterWrongPasswords = 0;
+
+            for (int i = passwordInput.Length - 1; i >= 0; i--)
+            {
+                reversedInput += passwordInput[i];
+            }
+            reversedInput.ToString();
+
+            bool passwordIsFalse = true;
+
+            while (passwordIsFalse)
+            {
+                string readPassword = Console.ReadLine();
+
+                if (readPassword == reversedInput)
+                {
+                    Console.WriteLine($"User {passwordInput} logged in.");
+                    break;
+                }
+                else if (readPassword != reversedInput)
+                {
+                    Console.WriteLine("Incorrect password. Try again.");
+                  
+                    counterWrongPasswords++;
+                }
+                if (counterWrongPasswords > 2)
+                {
+                    Console.WriteLine($"User {passwordInput} blocked!");
+                    break;
+                }
+            }
+
+        }
+    }
+}
