85071ce1df7a671a906db9982f57aba8131759d4
Nikolay Karadzhov <35952928+NikolayKaradzhov@users.noreply.github.com>
2019-01-28 09:32:10 +0200

    Update 01.Student Information.cs

--- Intro and Basic Syntax - Lab/01.Student Information.cs
+++ Intro and Basic Syntax - Lab/01.Student Information.cs
@@ -1,1 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
+namespace ConsoleApp23
+{
+    class Program
+    {
+        static void Main()
+        {
+            string name = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+            double grade = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Name: {name}, Age: {age}, Grade: {grade:F2}");
+        }
+    }
+}
