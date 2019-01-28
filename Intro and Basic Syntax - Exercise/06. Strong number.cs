943f0bf0822dc35093184be5a0c41e6c6028c7b0
Nikolay Karadzhov <35952928+NikolayKaradzhov@users.noreply.github.com>
2019-01-28 11:14:24 +0200

    Update 06. Strong number.cs

--- Intro and Basic Syntax - Exercise/06. Strong number.cs
+++ Intro and Basic Syntax - Exercise/06. Strong number.cs
@@ -1,1 +1,41 @@
+using System;
+using System.Globalization;
+using System.Linq;
 
+class TriangleOfNumbers
+{
+    static void Main()
+    {
+        int number = int.Parse(Console.ReadLine());
+
+        int newNumber = number;
+
+        int sum = 0;
+
+        while (number > 0)
+        {
+            int lastDigit = number % 10;
+
+            number = number / 10;
+
+            int factoriel = 1;
+
+            for (int i = 1; i <= lastDigit; i++)
+            {
+                factoriel = factoriel * i;
+            }
+            sum = sum + factoriel;
+        }
+
+        bool isStrong = sum == newNumber;
+
+        if (isStrong)
+        {
+            Console.WriteLine("yes");
+        }
+        else
+        {
+            Console.WriteLine("no");
+        }
+    }
+}
