2a207a146158a8bcb2ae6152e23605a6d376d135
Nikolay Karadzhov <35952928+NikolayKaradzhov@users.noreply.github.com>
2019-01-28 09:59:55 +0200

    Update 10.Multiplication Table.cs

--- Intro and Basic Syntax - Lab/10.Multiplication Table.cs
+++ Intro and Basic Syntax - Lab/10.Multiplication Table.cs
@@ -1,1 +1,19 @@
+using System;
 
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= 10; i++)
+            {
+                int result = input * i;   
+                    
+                    Console.WriteLine($"{input} X {i} = {result}");
+            }
+        }
+    }
+}
