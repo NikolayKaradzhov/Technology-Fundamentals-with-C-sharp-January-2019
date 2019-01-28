90c88ca350dd87b6c007c77f82d5aa66704cdb6c
Nikolay Karadzhov <35952928+NikolayKaradzhov@users.noreply.github.com>
2019-01-28 09:55:12 +0200

    Update 03.Passed or Failed.cs

--- Intro and Basic Syntax - Lab/03.Passed or Failed.cs
+++ Intro and Basic Syntax - Lab/03.Passed or Failed.cs
@@ -1,1 +1,18 @@
-
+using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+		double grade = double.Parse(Console.ReadLine());
+		
+		if(grade > 2.99)
+		{
+			Console.WriteLine("Passed!");		
+		}
+		else 
+		{
+			Console.WriteLine("Failed!");
+		}
+	}
+}
