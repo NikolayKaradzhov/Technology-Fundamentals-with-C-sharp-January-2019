873bf797a99d47b5ca435a409b2b6fc0ab63fa60
Nikolay Karadzhov <35952928+NikolayKaradzhov@users.noreply.github.com>
2019-01-28 11:17:40 +0200

    Update 04. Reverse String.cs

--- Intro and Basic Syntax - More Exercise/04. Reverse String.cs
+++ Intro and Basic Syntax - More Exercise/04. Reverse String.cs
@@ -1,1 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            for (int i = input.Length - 1; i >= 0; i--)
+            {
+                Console.Write(input[i]);
+            }
+            Console.WriteLine();
+        }
+    }
+}
