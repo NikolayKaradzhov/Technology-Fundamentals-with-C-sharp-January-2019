8c0c36281f40a63f8d3602dbe204a5618ffbdcb7
Nikolay Karadzhov <35952928+NikolayKaradzhov@users.noreply.github.com>
2019-01-28 11:18:10 +0200

    Update 05. Messages.cs

--- Intro and Basic Syntax - More Exercise/05. Messages.cs
+++ Intro and Basic Syntax - More Exercise/05. Messages.cs
@@ -1,1 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int typingRows = int.Parse(Console.ReadLine());
+         
+            int numberOfDigits = 0;
+
+            string message = string.Empty;            
+
+            for (int i = 1; i <= typingRows; i++)
+            {
+                int n = int.Parse(Console.ReadLine());
+
+                numberOfDigits = n.ToString().Length;
+
+                int mainDigit = n % 10;
+                int offsetOfTheNumber = (mainDigit - 2) * 3;
+
+                if (mainDigit == 8 || mainDigit == 9) 
+                {
+                    offsetOfTheNumber++;
+                }
+                int letterIndex = offsetOfTheNumber + numberOfDigits - 1;
+
+                if (mainDigit != 0)
+                {
+                    message += (char)(letterIndex + 97);
+                }
+                else
+                {
+                    message += (char)(32);
+                }
+            }
+            Console.WriteLine(message);
+        }
+    }
+}
