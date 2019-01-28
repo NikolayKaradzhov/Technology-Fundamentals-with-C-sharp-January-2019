dd3c447b624cc771c4c440a88dcf4b1f60049f87
Nikolay Karadzhov <35952928+NikolayKaradzhov@users.noreply.github.com>
2019-01-28 09:59:10 +0200

    Update 08.Divisible by 3.cs

--- Intro and Basic Syntax - Lab/08.Divisible by 3.cs
+++ Intro and Basic Syntax - Lab/08.Divisible by 3.cs
@@ -1,1 +1,15 @@
+using System;
 
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 3; i <= 100; i+=3)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
