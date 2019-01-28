6e04d50041651a1fdf841bad677ad41617581fcc
Nikolay Karadzhov <35952928+NikolayKaradzhov@users.noreply.github.com>
2019-01-28 11:25:04 +0200

    Create  08. Town Info.cs

---
+++ Data Types and Variables - Lab/08. Town Info.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace p08.Town_Info
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nameOfTown = Console.ReadLine();
+
+            int population = int.Parse(Console.ReadLine());
+
+            int area = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Town {nameOfTown} has population of {population} and area {area} square km.");
+        }
+    }
+}
