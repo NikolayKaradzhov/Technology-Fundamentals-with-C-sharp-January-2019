fa697b11e048bbe735d7b4553cb41801c3e895f5
Nikolay Karadzhov <35952928+NikolayKaradzhov@users.noreply.github.com>
2019-01-28 09:56:27 +0200

    Update 06.Foreign Languages.cs

--- Intro and Basic Syntax - Lab/06.Foreign Languages.cs
+++ Intro and Basic Syntax - Lab/06.Foreign Languages.cs
@@ -1,1 +1,22 @@
-
+using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+		string country = Console.ReadLine();
+		
+		if(country.Equals("England") || country.Equals("USA")) 
+		{
+			Console.WriteLine("English");
+		}
+		else if(country.Equals("Spain") || country.Equals("Argentina") || country.Equals("Mexico"))
+		{
+			Console.WriteLine("Spanish");
+		}
+		else
+		{
+			Console.WriteLine("unknown");
+		}
+	}
+}
