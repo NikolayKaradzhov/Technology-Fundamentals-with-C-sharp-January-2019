7bae7fc9dea3c03383f00283e9981df68258640f
Nikolay Karadzhov <35952928+NikolayKaradzhov@users.noreply.github.com>
2019-01-28 11:15:32 +0200

    Update 09. Padawan Equipment.cs

--- Intro and Basic Syntax - Exercise/09. Padawan Equipment.cs
+++ Intro and Basic Syntax - Exercise/09. Padawan Equipment.cs
@@ -1,1 +1,36 @@
+using System;
+using System.Globalization;
 
+class TriangleOfNumbers
+{
+    static void Main()
+    {
+        double amountOfMoneyIvanChoHas = double.Parse(Console.ReadLine());
+        int studentsCount = int.Parse(Console.ReadLine());
+
+        double lightSabrePrice = double.Parse(Console.ReadLine());
+        double robesPrice = double.Parse(Console.ReadLine());
+        double beltPrice = double.Parse(Console.ReadLine());
+
+        double lightSabre10PercentMore = Math.Ceiling((studentsCount * 0.10) + studentsCount);
+        double lightSabreTotalPrice = lightSabrePrice * lightSabre10PercentMore;
+        double robesTotalPrice = robesPrice * studentsCount;
+        double freeBelts = Math.Floor(studentsCount / 6.0);
+        double beltsTotalPrice = (studentsCount - freeBelts) * beltPrice;
+
+        double totalPriceAll = lightSabreTotalPrice + robesTotalPrice + beltsTotalPrice;
+
+        double neededMoney = 0.0;
+
+        if (amountOfMoneyIvanChoHas >= totalPriceAll)
+        {
+            Console.WriteLine($"The money is enough - it would cost {totalPriceAll:F2}lv.");
+        }
+        else if (totalPriceAll > amountOfMoneyIvanChoHas)
+        {
+            neededMoney = totalPriceAll - amountOfMoneyIvanChoHas;
+            Console.WriteLine($"Ivan Cho will need {neededMoney:F2}lv more.");
+        }
+
+    }
+}
