cf9c0e100dd67a393ca140ef97744c18f0cf7ca0
Nikolay Karadzhov <35952928+NikolayKaradzhov@users.noreply.github.com>
2019-01-28 11:12:57 +0200

    Update 04.Print and sum.cs

--- Intro and Basic Syntax - Exercise/04.Print and sum.cs
+++ Intro and Basic Syntax - Exercise/04.Print and sum.cs
@@ -1,1 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int startNumber = int.Parse(Console.ReadLine());
+            int endNumber = int.Parse(Console.ReadLine());
+
+            int totalSum = 0;
+
+            for (int i = startNumber; i <= endNumber; i++)
+            {
+                totalSum += i;
+                Console.Write(i + " ");      
+            }
+            Console.WriteLine();
+            Console.WriteLine($"Sum: {totalSum}");
+        }
+    }
+}
