cc4ce5ec2fe82d585be2f330aed36b761b11f8a2
Nikolay Karadzhov <35952928+NikolayKaradzhov@users.noreply.github.com>
2019-01-28 09:32:49 +0200

    Update 02.Passed.cs

--- Intro and Basic Syntax - Lab/02.Passed.cs
+++ Intro and Basic Syntax - Lab/02.Passed.cs
@@ -1,1 +1,14 @@
-
+using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+		double grade = double.Parse(Console.ReadLine());
+		
+		if(grade >= 3)
+		{
+			Console.WriteLine("Passed!");		
+		}
+	}
+}
