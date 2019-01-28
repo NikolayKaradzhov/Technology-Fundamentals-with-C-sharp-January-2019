ecbe7080cc3a0b5f9c5dda12c6decc3b46f4902f
Nikolay Karadzhov <35952928+NikolayKaradzhov@users.noreply.github.com>
2019-01-28 11:22:05 +0200

    Update 01. Convert Meters to Kilometers.cs

--- Data Types and Variables - Lab/01. Convert Meters to Kilometers.cs
+++ Data Types and Variables - Lab/01. Convert Meters to Kilometers.cs
@@ -1,1 +1,16 @@
+using System;
 
+namespace p01.Convert_meters_to_kilometers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int metersDistance = int.Parse(Console.ReadLine());
+
+            double resultInKilometers = metersDistance / 1000.00;
+
+            Console.WriteLine($"{resultInKilometers:F2}");
+        }
+    }
+}
