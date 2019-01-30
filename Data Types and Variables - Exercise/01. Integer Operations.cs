869159d6e62f4fc5c8f1011beb5f990521f1068c
Nikolay Karadzhov <35952928+NikolayKaradzhov@users.noreply.github.com>
2019-01-30 22:27:32 +0200

    Update 01. Integer Operations.cs

--- Data Types and Variables - Exercise/01. Integer Operations.cs
+++ Data Types and Variables - Exercise/01. Integer Operations.cs
@@ -1,1 +1,20 @@
+using System;
 
+namespace p01.Integer_Operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+            int thirdNumber = int.Parse(Console.ReadLine());
+            int fourthNumber = int.Parse(Console.ReadLine());
+
+            int midResult = (firstNumber + secondNumber) / thirdNumber;
+            int endResult = midResult * fourthNumber;
+
+            Console.WriteLine(endResult);
+        }
+    }
+}
