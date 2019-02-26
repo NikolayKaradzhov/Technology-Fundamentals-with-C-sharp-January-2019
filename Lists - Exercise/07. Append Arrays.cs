7c11e2c7d716b84c023fe3306b2e0563319ac98b
Nikolay Karadzhov <35952928+NikolayKaradzhov@users.noreply.github.com>
2019-02-26 16:30:01 +0200

    Update 07. Append Arrays.cs

--- Lists - Exercise/07. Append Arrays.cs
+++ Lists - Exercise/07. Append Arrays.cs
@@ -1,1 +1,25 @@
-
+using System;
+using System.Collections.Generic;
+using System.Linq;
+ 
+namespace p07.Append_Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+ 
+            List<string> numbers = Console.ReadLine().Split('|', StringSplitOptions.RemoveEmptyEntries).Reverse().ToList();
+          
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                List<string> temp = numbers[i].Split(" ", StringSplitOptions.RemoveEmptyEntries).ToList();
+          
+                for (int j = 0; j < temp.Count; j++)
+                {
+                    Console.Write($"{temp[j]} ");
+                }
+            }
+        }
+    }
+}
