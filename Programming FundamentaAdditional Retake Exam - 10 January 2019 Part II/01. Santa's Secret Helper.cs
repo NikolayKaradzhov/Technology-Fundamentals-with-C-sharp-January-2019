3f511fedf4a840af64326c2fef05a4ba9c70ffb4
Nikolay Karadzhov <35952928+NikolayKaradzhov@users.noreply.github.com>
2019-04-05 15:19:31 +0300

    Create 01. Santa's Secret Helper.cs

---
+++ Programming FundamentaAdditional Retake Exam - 10 January 2019 Part II/01. Santa's Secret Helper.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace 01.Santas Secret Helper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"@(?<name>[A-Za-z]+)[^@\-!:>]+!(?<gOrN>[G|N])!";
+
+            int key = int.Parse(Console.ReadLine());
+
+            string message = Console.ReadLine();
+            string newMessage = string.Empty;
+            string kidsName = string.Empty;
+
+            string gORn = string.Empty;
+
+            List<string> goodKids = new List<string>();
+
+            while (message != "end")
+            {
+                newMessage = string.Empty;
+
+                foreach (var character in message)
+                {
+                    newMessage += (char)(character - key);
+                }
+
+                Regex order = new Regex(pattern);
+
+                MatchCollection matches = order.Matches(newMessage);
+
+                foreach (Match item in matches)
+                {
+                    kidsName = item.Groups["name"].Value;
+                    gORn = item.Groups["gOrN"].Value;
+
+                    if (gORn == "G")
+                    {
+                        goodKids.Add(kidsName);
+                    }
+                }
+
+                message = Console.ReadLine();
+            }
+
+            foreach (var name in goodKids)
+            {
+                Console.WriteLine(name);
+            }
+        }
+    }
+}
