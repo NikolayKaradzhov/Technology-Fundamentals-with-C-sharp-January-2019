26c411e9efa6ba8afd582fb9675d7e4dc9d1d3ec
Nikolay Karadzhov <35952928+NikolayKaradzhov@users.noreply.github.com>
2019-04-08 23:26:45 +0300

    Create 02.SoftUni Exam Results.cs

---
+++ Programming Fundamentals Exam - 01 July 2018 Part II/02.SoftUni Exam Results.cs
@@ -0,0 +1,82 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace p02.SoftUni_Exam_Results
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> results = new Dictionary<string, int>();
+            Dictionary<string, int> submissions = new Dictionary<string, int>();
+
+            string input = Console.ReadLine();
+
+            while (input != "exam finished")
+            {
+                List<string> tokens = input.Split("-").ToList();
+                string userName = tokens[0];
+                string language = tokens[1];
+                
+                if (language == "banned")
+                {
+                    results.Remove(userName);
+                }
+                else
+                {
+                    int points = int.Parse(tokens[2]);
+
+                    if (!results.ContainsKey(userName) && !submissions.ContainsKey(language))
+                    {
+                        results.Add(userName, points);
+                        submissions.Add(language, 1);
+                    }
+                    else if (!results.ContainsKey(userName) && submissions.ContainsKey(language))
+                    {
+                        results.Add(userName, points);
+                        submissions[language]++;
+                    }
+                    else if (results.ContainsKey(userName) && !submissions.ContainsKey(language))
+                    {
+                        submissions[language]++;
+                    }
+                    else if (results.ContainsKey(userName)
+                        && submissions.ContainsKey(language)
+                        && results[userName] < points)
+                    {
+                        results[userName] = points;
+                        submissions[language]++;
+                    }
+                    else if (results.ContainsKey(userName)
+                        && submissions.ContainsKey(language)
+                        && results[userName] > points)
+                    {
+                        submissions[language]++;
+                    }
+                }
+                input = Console.ReadLine();
+            }
+            var resultsOrderedByDescending = results
+                .OrderByDescending(x => x.Value)
+                .ThenBy(x => x.Key);
+
+            Console.WriteLine("Results:");
+
+            foreach (var item in resultsOrderedByDescending)
+            {
+                Console.WriteLine($"{item.Key} | {item.Value}");
+            }
+            var submissionsOrdered = submissions
+                .OrderByDescending(x => x.Value)
+                .ThenBy(x => x.Key);
+
+            Console.WriteLine("Submissions:");
+
+            foreach (var item in submissionsOrdered)
+            {
+                Console.WriteLine($"{item.Key} - {item.Value}");
+            }
+        }
+    }
+}
